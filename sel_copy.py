e91164fba178e934e1306a8efb4c412714bcfb59
mrdionjr <mr.dionjr@gmail.com>
2017-07-24 15:51:19 +0000

    Copy files to a new folder

--- sel_copy.py
+++ sel_copy.py
@@ -1,19 +1,31 @@
 import os, shutil
 
 
-def selective_copy(folder, extension, copy_folder=''):
-    folder = os.path.abspath(folder)
-    if extension.startswith('.'):
-        copy_folder = extension[1:] + '_folder'
+def selective_copy(folder, extension, copy_folder=None):
+    """."""
+    folder = os.path.abspath(folder)  # Get the absolute path
+
+    #  Create a new folder if the copy folder does not exists
+    if copy_folder:
+        if not os.path.exists(os.path.join(folder, copy_folder)):
+            os.makedirs(os.path.join(folder, copy_folder))
     else:
-        copy_folder = extension + '_folder'
-    if not os.path.exists(copy_folder):
-        os.makedirs(copy_folder)
+        if extension.startswith('.'):
+            copy_folder = extension[1:] + '_folder'
+        else:
+            copy_folder = extension + '_folder'
+        try:
+            os.makedirs(os.path.join(folder, copy_folder))
+        except FileExistsError:
+            print('A folder called "{}" will be used.'.format(copy_folder))
+
+    #  Walk through the entire folder and copy the corresponding files
     for foldername, subfolders, filenames in os.walk(folder):
         for filename in filenames:
             if filename.endswith(extension):
-                shutil.copy(filename, copy_folder)
+                print('Copying {} to {}...'.format(filename, copy_folder))
+                shutil.copy(os.path.join(foldername, filename), copy_folder)
     print('Done.')
 
 
-selective_copy('.', '.txt')
+selective_copy('.', '.txt', 'my_file')
