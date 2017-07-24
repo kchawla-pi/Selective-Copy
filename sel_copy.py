abd733cd6ae9183c508979800edcd16195932df4
mrdionjr <mr.dionjr@gmail.com>
2017-07-24 13:44:13 +0000

    Support for current directory

--- sel_copy.py
+++ sel_copy.py
@@ -1,4 +1,19 @@
 import os, shutil
 
-def selective_copy(folder, extension):
-    pass
+
+def selective_copy(folder, extension, copy_folder=''):
+    folder = os.path.abspath(folder)
+    if extension.startswith('.'):
+        copy_folder = extension[1:] + '_folder'
+    else:
+        copy_folder = extension + '_folder'
+    if not os.path.exists(copy_folder):
+        os.makedirs(copy_folder)
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            if filename.endswith(extension):
+                shutil.copy(filename, copy_folder)
+    print('Done.')
+
+
+selective_copy('.', '.txt')
